f0194b081ce5e6ad7171bb1fc2fbf20d5fc2b137
nox <ijmermet@gmail.com>
2020-02-07 01:36:41 -0300

    shut hook

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-git init
-cd reveal.js
-git submodule init
-git submodule update
-git checkout version 3.5.0
-cd ..
-git submodule add https://github.com/hakimel/reveal.js reveal.js
-git add .
-git commit -m "initial structure"
+#git init
+#cd reveal.js
+#git submodule init
+#git submodule update
+#git checkout version 3.5.0
+#cd ..
+#git submodule add https://github.com/hakimel/reveal.js reveal.js
+#git add .
+#git commit -m "initial structure"
