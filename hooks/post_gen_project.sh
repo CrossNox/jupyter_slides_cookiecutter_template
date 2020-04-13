4615ae2ee42165d5e8b55e9a83d9412db1a9fe97
CrossNox <ijmermet@gmail.com>
2020-04-12 21:26:05 -0300

    use https to clone reveal.js

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-git clone -b '3.5.0' --single-branch --depth 1 git@github.com:hakimel/reveal.js.git
+git clone -b '3.5.0' --single-branch --depth 1 https://github.com/hakimel/reveal.js.git
 git init
-git submodule add git@github.com:hakimel/reveal.js.git reveal.js
+git submodule add https://github.com/hakimel/reveal.js.git reveal.js
 git add .
 git commit -m "initial structure"
